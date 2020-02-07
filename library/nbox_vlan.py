2d01ef60449770db0286b8d11a19ff3c77439fb2
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-02-07 16:30:50 -0500

    Lingering changes

---
+++ library/nbox_vlan.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# (c) 2019, Larry Smith Jr. <mrlesmithjr@gmail.com>
+#
+# This file is a module for managing NetBox vrfs
+
+from ansible.module_utils.basic import *
+import json
+import requests
